2d756f74ccf4af1456cc74771160cccd63ef8b5e
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-06-25 15:21:00 +0200

    Parameterized mark_glosses.sh

--- not-to-release/mark_glosses.sh
+++ not-to-release/mark_glosses.sh
@@ -1,3 +1,4 @@
-perl -pe 's/^(\d+\t(?:[^\t]+\t){8})([^\t]+)$/$1Gloss=$2/' < yo_ytb-ud-test.conllu > fixed1.conllu
+# Usage: mark_glosses.sh yo_ytb-ud-test.conllu
+perl -pe 's/^(\d+\t(?:[^\t]+\t){8})([^\t]+)$/$1Gloss=$2/' < $1 > fixed1.conllu
 treex -Lyo Read::CoNLLU from=fixed1.conllu W2W::InferNoSpaceAfterFromText Write::CoNLLU to=fixed2.conllu print_zone_id=0
 
